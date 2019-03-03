1a96df0c55f8fee24bc502a38b71f658826d133d
xGqrz <35878604+xGqrz@users.noreply.github.com>
2019-03-03 08:53:07 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,29 +1,77 @@
-const Discord = require('discord.js');
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
 
-const client = new Discord.Client();
+const fs = require("fs");
 
- 
+const bot = new discord.Client();
+bot.commands = new discord.Collection();
 
-client.on('ready', () => {
+fs.readdir("./commands/", (err, files) => {
 
-    console.log('I am ready!');
+  if (err) console.log(err);
+
+  var jsFiles = files.filter(f => f.split(".").pop() === "js");
+
+  if (jsFiles.length <= 0) {
+    console.log("Geen files gevonden");
+    return;
+  }
+
+  jsFiles.forEach((f, i) => {
+
+    var fileGet = require(`./commands/${f}`);
+    console.log(`De file ${f} is geladen`);
+
+    bot.commands.set(fileGet.help.name, fileGet);
+
+  })
+
 
 });
 
- 
+bot.on("ready", async () => {
 
-client.on('message', message => {
+  console.log(`${bot.user.username} is online!`)
+  bot.user.setActivity('-new | -help', { type: 'WATCHING' })
 
-    if (message.content === 'ping') {
+});
+  
+bot.on("guildMemberAdd", member => {
+   
+  var role = member.guild.roles.find("name","👉Bezoeker");
+  console.log(role)
+  member.addRole(role.id);
+  
+})
 
-       message.reply('pong');
+bot.on("message", async message => {
+  if (message.author.bot) return;
 
-       }
+  if (message.channel.type === "dm") return;
+
+  var prefixes = JSON.parse(fs.readFileSync("./prefixes.json"));
+  if (!prefixes[message.guild.id]){
+    prefixes[message.guild.id] = {
+    prefixes: botConfig.prefix
+  };
+
+  }
+  var prefix = prefixes[message.guild.id].prefixes;
+  // var prefix = botConfig.prefix;
+  if (!message.content.startsWith(prefix)) return;
+  if (message.content.startsWith("trigger")) return;
+
+  var messageArray = message.content.split(" ");
+
+
+  var command = messageArray[0];
+
+  var arguments = messageArray.slice(1);
+
+  var commands = bot.commands.get(command.slice(prefix.length));
+
+  if(commands) commands.run(bot,message, arguments);
 
 });
 
- 
-
-// THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+bot.login(botConfig.token);
---
+++ support.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) =>{
+
+
+        message.channel.send(":point_right:Zie je prive berichten!");
+        message.author.send("De supportserver is: https://discordapp.com/invite/28UFpur");
+        message.author.send("Discord-Bot: Gerland: https://discordapp.com/oauth2/authorize?client_id=520971204467884032&scope=bot");
+      return;
+    }
+
+module.exports.help = {
+    name: "support"
+
+    
+}
